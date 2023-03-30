d6f3abd22f7226053c364f6651d23f01d32994e8
つるつる <hinataptyan@gmail.com>
2023-03-30 23:13:21 +0000

    ボードの状態を記録する

--- src/App.js
+++ src/App.js
@@ -8,6 +8,22 @@ function Square({ value, onSquareClick }) {
   );
 }
 
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
+  );
+}
+
 function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
@@ -25,19 +41,6 @@ function Board() {
     setSquares(nextSquares);
     setXIsNext(!xIsNext);
   }
-  
-  export default function Game() {
-  return (
-    <div className="game">
-      <div className="game-board">
-        <Board />
-      </div>
-      <div className="game-info">
-        <ol>{/*TODO*/}</ol>
-      </div>
-    </div>
-  );
-}
 
   function calculateWinner(squares) {
     const lines = [
